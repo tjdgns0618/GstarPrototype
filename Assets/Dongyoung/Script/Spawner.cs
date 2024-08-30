9278ca9ac0bfe60bae606351795539e56d7087a3
dykim289 <143568504+dykim289@users.noreply.github.com>
2024-08-30 17:52:24 +0900

    Spawner 초반 작업
    Spawner 초반 작업

---
+++ Assets/Dongyoung/Script/Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Spawner : MonoBehaviour
+{
+    public Wave[] waves;
+
+    Wave currentWave;
+    int currentWaveNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Dongyoung/Script/Wave.cs
@@ -0,0 +1,10 @@
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave : MonoBehaviour
+{
+    public GameObject enemy;
+    public int count; //积己瞪 enemy 箭磊
+    public float rate; //积己 林扁
+}
