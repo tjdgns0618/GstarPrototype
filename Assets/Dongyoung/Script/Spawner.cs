c3ac02fec30b39e45ad13461857adc6dddc3a315
parknayun <nayun3454@gmail.com>
2024-09-02 09:07:36 +0900

    spawn 중간

--- Assets/Dongyoung/Script/Spawner.cs
+++ Assets/Dongyoung/Script/Spawner.cs
@@ -6,9 +6,15 @@ using System;
 public class Spawner : MonoBehaviour
 {
     public Wave[] waves;
+    //public Enemy enemy;
 
     Wave currentWave;
-    int currentWaveNumber;
+    int currentWaveNumber; //현재 웨이브
+
+    int enemiesRemainingToSpawn; //남은 소환할 적의 수
+    int enemiesRemainingAlive; //살아있는 적의 수
+    float nextSpawnTime; //다음 소환 시간
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +24,20 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemiesRemainingToSpawn > 0 %% Time.time > nextSpawnTime)
+        {
+            enemiesRemainingToSpawn--;
+            nextSpawnTime = Time.time - currentWave.timeBetweenSpawn;
+
+            //Enemy spawnedEnemy = Instantiate(spawnedEnemy, Vector3.zero,Quaternion.identity);
+            spawnEnemy OnDeath += Onenemy
+        }
+    }
+
+    [Serializable]
+    public class Wave
+    {
+        public int enemyCount; //소환할 적의 수
+        public float timeBetweenSpawn; // 소환 딜레이
     }
 }
