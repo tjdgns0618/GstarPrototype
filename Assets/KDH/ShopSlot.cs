fca67b995f579b6b167617602cb3b437f9331458
donghyun <kdh9438@naver.com>
2024-09-04 12:42:07 +0900

    슬롯 관련 스크립트 추가

--- Assets/KDH/ShopSlot.cs
+++ Assets/KDH/ShopSlot.cs
@@ -5,12 +5,17 @@ using UnityEngine.EventSystems;
 
 public class ShopSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public ItemManager _item;
+    public SlotToolTip _slotToolTip;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-
+        if (_item != null)
+            _slotToolTip.ShowToolTip(_item, transform.position);
     }
+
     public void OnPointerExit(PointerEventData eventData)
     {
-
+        _slotToolTip.HideToolTip();
     }
 }
