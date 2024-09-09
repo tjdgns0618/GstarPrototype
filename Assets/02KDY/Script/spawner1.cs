a609d9767fcc5acdae468741c7fda614d8c4fc7a
GodDr02 <dykim289@gmail.com>
2024-09-09 11:42:19 +0900

    스포너 업데이트

--- Assets/02KDY/Script/spawner1.cs
+++ Assets/02KDY/Script/spawner1.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class spawner1 : MonoBehaviour
 {
     public List<GameObject> enemyprefab; //enemy프리팹
     public Transform player; // 플레이어의 위치
+    public GameObject portal;
+    public GameObject rewardUI;
 
     public float spawnRadius = 20f; // 플레이어로부터 enemy가 생성될 수 있는 최대 거리
     public float minDistancefromPlayer = 5f; // 플레이어와 enemy 간의 최소 거리
@@ -14,8 +17,8 @@ public class spawner1 : MonoBehaviour
     public float spawnInterval = 3f; //enemy 생성 주기
     public float waveInterval = 5f; //웨이브 간 대기 시간
     public int maxWaves = 5; //최대 웨이브 수
+    public int currentWave = 0; //현재 웨이브
 
-    private int currentWave = 0; //현재 웨이브
     private int enemyPerSpawn; //한 번의 주기에 생성할 enemy 수
     private int spawnedCount = 0; //생성된 enemy 카운트
     private int totalEnemiesInWave; //현재 웨이브에서 생성할 enemy의 전체 수
@@ -31,6 +34,7 @@ public class spawner1 : MonoBehaviour
 
     void StartWave()
     {
+        Time.timeScale = 1;
         StartCoroutine(WaveSystem());
     }
 
@@ -41,10 +45,18 @@ public class spawner1 : MonoBehaviour
 
         if (spawnedCount == 0)
         {
-            StartWave();
+            Time.timeScale = 0;
+            rewardUI.SetActive(true);
         }
     }
 
+    //void NextStage()
+    //{
+    //    if(currentWave == maxWaves && spawnedCount == 0)
+    //    {
+    //        // 포탈 활성화
+    //    }
+    //}
     IEnumerator WaveSystem() //웨이브 시스템
     {
         if (currentWave < maxWaves)
