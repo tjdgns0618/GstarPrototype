43b097d1d6b2828238869559ee5ca21d2caf00aa
dykim289 <143568504+dykim289@users.noreply.github.com>
2024-09-10 21:39:58 +0900

    enemy를 카운트 하는 기능 추가
    되는지 안되는지는 아직 모름..

--- Assets/02KDY/Script/spawner1.cs
+++ Assets/02KDY/Script/spawner1.cs
@@ -10,10 +10,10 @@ public class spawner1 : MonoBehaviour
 {
     public List<GameObject> enemyprefab; //enemy프리팹
     public Transform player; // 플레이어의 위치
-    public GameObject portal;
+    //public GameObject portal;
     public GameObject rewardUI;
-    public GameObject playerPrefab;
-
+    //public GameObject playerPrefab;
+    public TMP_Text waveInfoText;
 
     public float spawnRadius = 20f; // 플레이어로부터 enemy가 생성될 수 있는 최대 거리
     public float minDistancefromPlayer = 5f; // 플레이어와 enemy 간의 최소 거리
@@ -26,6 +26,7 @@ public class spawner1 : MonoBehaviour
     private int enemyPerSpawn; //한 번의 주기에 생성할 enemy 수
     private int spawnedCount = 0; //생성된 enemy 카운트
     private int totalEnemiesInWave; //현재 웨이브에서 생성할 enemy의 전체 수
+    private int enemiesLeft;
 
     public delegate void Action();
     public Action enemyDead;
@@ -81,6 +82,8 @@ public class spawner1 : MonoBehaviour
         enemyPerSpawn = firstWaveEnemy + (currentWave - 1) * 2; //웨이브마다 생성할 몬스터 수 증가
         totalEnemiesInWave = enemyPerSpawn * 2; //웨이브마다 총 생성할 몬스터의 수(지금은 웨이브 당 4마리씩 증가 ex.10->14->18)
         spawnedCount = 0;
+        enemiesLeft = totalEnemiesInWave;
+        UpdateWaveInfoUI();
         Debug.Log($"{currentWave}웨이브 시작! 적 {totalEnemiesInWave}개 생성");
     }
 
@@ -128,5 +131,14 @@ public class spawner1 : MonoBehaviour
             //플레이어 위치를 중심해서 스폰 반경 표시
             Gizmos.DrawWireSphere(player.position, spawnRadius);
         }
+    }
+    public void OnEnemyDestroyed()
+    {
+        enemiesLeft--;
+        UpdateWaveInfoUI();
+    }
+    void UpdateWaveInfoUI()
+    {
+        waveInfoText.text = $" Enemies Left : {enemiesLeft}";
     }
 }
