09b56e6abac551e31e81f902cfc36ae7da3bd13e
Suhwan <suhyoun222@gmail.com>
2024-10-25 12:38:39 +0900

    soundmanager추가

--- Assets/04YJE/02Scripts/Sound/SoundManager.cs
+++ Assets/04YJE/02Scripts/Sound/SoundManager.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
+
+    public AudioMixer audioMixer;
+
+    public AudioSource bgmSource;
+    public AudioClip[] bgmLists;
 
     private void Awake()
     {
@@ -14,13 +21,40 @@ public class SoundManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    void Start()
+    private void Start()
     {
-        
+        SetSFXVolume(10);
+        SetBGMVolume(-80);
     }
 
-    void Update()
+    public void PlayMusic(int index, float fadeDuration = 0.5f)
     {
-        
+        bgmSource.clip = bgmLists[index];
+    }
+
+    public void MuteBGM()
+    {
+        bgmSource.mute = true;
+    }
+
+    public void UnmuteBGM()
+    {
+        bgmSource.mute = false;
+    }
+
+    public void MuteSFX()
+    {
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        if (audioMixer != null)
+            audioMixer.SetFloat("SFX", volume);
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        if(audioMixer != null)
+            audioMixer.SetFloat("BGM", volume);
     }
 }
