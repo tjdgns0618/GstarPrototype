329a3f2c7b68f172956f6f41a19a47bea4de799e
유지은 <120632525+jey275837@users.noreply.github.com>
2024-10-28 00:37:25 +0900

    폰트추가, 대미지 팝업 제작중..

--- Assets/04YJE/02Scripts/UI/DamageText.cs
+++ Assets/04YJE/02Scripts/UI/DamageText.cs
@@ -3,25 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DamageText : MonoBehaviour
 {
-    [SerializeField] private TMP_Text damage_Text;
     [SerializeField] private GameObject cam;
+
+    private TMP_Text damage_Text;
+    private Transform _transform;
+    private Animator animator;
+
+    private void Start()
+    {
+        _transform = transform;
+        animator = GetComponent<Animator>();
+        damage_Text = GetComponent<TMP_Text>();
+    }
 
     private void OnEnable()
     {
-        Destroy(gameObject, 2f);
     }
 
-    public void Init(float damage, Vector3 pos)
+    public void Init(float damage, Vector3 pos, bool isCritical)
     {
         damage_Text.text = ((int)damage).ToString();
-        transform.position = pos;
+        _transform.position = pos;
+        animator.SetBool("IsCritical", isCritical);
     }
 
     private void Update()
     {
-        transform.LookAt(cam.transform.position);
+        _transform.LookAt(cam.transform.position);
     }
 }
