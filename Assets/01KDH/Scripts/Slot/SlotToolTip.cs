33d50c43fbdcbf9f69040fb6375cfda8e92763d0
tjdgns0618 <111509499+GUEMHAE@users.noreply.github.com>
2024-11-06 19:39:14 +0900

    웨이브 몬스터 수 조절

--- Assets/01KDH/Scripts/Slot/SlotToolTip.cs
+++ Assets/01KDH/Scripts/Slot/SlotToolTip.cs
@@ -29,8 +29,12 @@ public class SlotToolTip : MonoBehaviour
         var itemData = itemDatabase.GetItem(_item.itemID);
         if(itemData.HasValue)
         {
-            float itemVariable = itemData.Value.text1_index;
-            txt_Itemeffect.text = string.Format(_item.itemEffect, itemVariable * _count);
+            float itemVariable = ItemDataBase.instance.Itemtext(_item.itemID);
+            float itemVariable2 = ItemDataBase.instance.Itemtext2(_item.itemID);
+            float itemVariable3 = ItemDataBase.instance.Itemtext3(_item.itemID);
+            float itemVariable4 = ItemDataBase.instance.Itemtext4(_item.itemID);
+
+            txt_Itemeffect.text = string.Format(_item.itemEffect, itemVariable, itemVariable2, itemVariable3, itemVariable4);
         }
     }
 
