3d1dd8b69b085ffab439367db805620e483f1a7b
Suhwan <nayun3454@gmail.com>
2024-11-11 10:31:10 +0900

    대미지,상점 수정

--- Assets/04YJE/02Scripts/UI/DamageText.cs
+++ Assets/04YJE/02Scripts/UI/DamageText.cs
@@ -28,7 +28,7 @@ public class DamageText : MonoBehaviour
 
     public void Init(float damage, Vector3 pos, bool isCritical)
     {
-        damage_Text.text = damage.ToString("{0:F2}");
+        damage_Text.text = damage.ToString("F2");
         _transform.position = pos;
         animator.SetBool("IsCritical", isCritical);
     }
